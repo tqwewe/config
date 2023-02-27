18ec46fdd47eafa28bc3067113d5c02eb6fe4600
Ari Seyhun <ariseyhun@live.com.au>
2023-02-27 23:27:13 +1030

    fix: mailserver module

--- modules/mailserver.nix
+++ modules/mailserver.nix
@@ -1,17 +1,26 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 
-{ inputs, ... }: inputs.mailserver.nixosModule {
-  enable = true;
+{ inputs, lib, pkgs, config, ... }: {
+  imports = [
+    inputs.mailserver.nixosModule
+  ];
 
-  fqdn = "mail.theari.dev";
-  domains = [ "theari.dev" ];
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "ariseyhun@live.com.au";
+  
+  mailserver = {
+    enable = true;
 
-  loginAccounts = {
-    "info@theari.dev" = {
-      hashedPassword = "$2b$05$SZOUIO97tCErijTOe60tWOU8ZmjJq7kQy9eWj70qKC7PH2s.Gs9qG";
+    fqdn = "mail.theari.dev";
+    domains = [ "theari.dev" ];
+
+    loginAccounts = {
+      "info@theari.dev" = {
+        hashedPassword = "$2b$05$SZOUIO97tCErijTOe60tWOU8ZmjJq7kQy9eWj70qKC7PH2s.Gs9qG";
+      };
     };
-  };
 
-  certificateScheme = 3;
+    certificateScheme = 3;
+  };
 }
