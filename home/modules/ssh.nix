5620f3143cfe2cad26fb8fa524f6ab848f9fe337
Ari Seyhun <ariseyhun@live.com.au>
2023-02-28 15:05:01 +1030

    feat: avoid connection dropping with server ssh

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -6,6 +6,10 @@
       server = {
         hostname = "139.180.178.235";
         user = "ari";
+        extraOptions = {
+          serverAliveInterval = "20";
+          TCPKeepAlive = "no";
+        };
       };
     };
   };
