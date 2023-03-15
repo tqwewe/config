a6a6aac743ed6041e983b4dcc5f5a0532bdcdfa4
Ari Seyhun <ariseyhun@live.com.au>
2023-03-15 14:38:37 +1030

    feat: add autoplay for librewolf

--- home/modules/librewolf.nix
+++ home/modules/librewolf.nix
@@ -4,6 +4,7 @@
 
     settings = {
       "identity.fxaccounts.enabled" = true;
+      "media.autoplay.blocking_policy" = 0;
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.downloads" = false;
       "webgl.disabled" = false;
