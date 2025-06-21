38e1b74bc2f551e19e9507cc2d356a037409681e
Ari Seyhun <dev@tqwewe.com>
2025-06-21 18:15:24 +1000

    feat: configure steam to use multiple threads for pre-shader processing

--- system/modules/steam.nix
+++ system/modules/steam.nix
@@ -10,6 +10,30 @@
   powerManagement.cpuFreqGovernor = "performance";
   hardware.xpadneo.enable = true;
 
+  # Systemd user service that auto-configures Steam shader threads
+  systemd.user.services.steam-shader-config = {
+    description = "Configure Steam shader preprocessing threads";
+    wantedBy = [ "default.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    script = ''
+      THREADS=$(${pkgs.coreutils}/bin/nproc)
+      STEAM_THREADS=$((THREADS - 4))
+
+      # Ensure minimum of 4 threads
+      if [ $STEAM_THREADS -lt 4 ]; then
+        STEAM_THREADS=4
+      fi
+
+      mkdir -p ~/.steam/steam
+      echo "unShaderBackgroundProcessingThreads $STEAM_THREADS" > ~/.steam/steam/steam_dev.cfg
+    '';
+  };
+
   environment.systemPackages = with pkgs; [
     protonup
   ];
